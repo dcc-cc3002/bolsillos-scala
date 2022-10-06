d511bd7a437d9b782e28198673dd9a58e1cd1910
R8V <ignacio.slater@ug.uchile.cl>
2022-10-05 22:50:37 -0300

    FEAT Implement Pokémon attack for types with "instanceof"

--- src/main/scala/GrassPokemon.scala
+++ src/main/scala/GrassPokemon.scala
@@ -1,5 +1,15 @@
-
 package cl.uchile.dcc.poke
 
 class GrassPokemon(name: String, maxHp: Int, strength: Int)
-  extends AbstractPokemon(name, maxHp, strength)
+  extends AbstractPokemon(name, maxHp, strength) {
+  override def attack(pokemon: Pokemon): Unit = {
+    pokemon match {
+      case _: FirePokemon =>
+        pokemon.currentHp -= strength / 10 * 1.5
+      case _: GrassPokemon =>
+        pokemon.currentHp -= strength / 10 * 0.5
+      case _ =>
+        pokemon.currentHp -= strength / 10
+    }
+  }
+}
